48385d9c12d670de25bf1978d179eb94c3eefd5d
Nashita Choudhury <nashitac9@gmail.com>
2023-06-19 13:29:23 +1000

    starting again

--- set3/exercise4.py
+++ set3/exercise4.py
@@ -25,22 +25,7 @@ def binary_search(low, high, actual_number):
     guess = 0
 
     # Write your code in here
-    tries = 0
-    while low < high:
-        guess = (low + high) // 2
-        print("guess: ", guess)
-        tries += 1
-        if guess == actual_number:
-            return {"guess": guess, "tries": tries}
-        if guess < actual_number:
-            low = guess + 1
-        else:
-            high = guess - 1
     return {"guess": guess, "tries": tries}
-
-    low = 0
-    high = 15
-    actual_number = 6
 
 
 
